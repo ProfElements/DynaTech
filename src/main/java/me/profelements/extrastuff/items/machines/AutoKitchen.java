7786f8f67dac1164a058d141ee5925d55c2ac45d
ProfElements <profelements1@gmail.com>
2020-12-17 18:51:09 -0600

    :recycle: fix(AutoKitchen): Refactored Auto-Kitchen to make it actually take Kitchen recipes

--- src/main/java/me/profelements/extrastuff/items/machines/AutoKitchen.java
+++ src/main/java/me/profelements/extrastuff/items/machines/AutoKitchen.java
@@ -1,12 +1,18 @@
 package me.profelements.extrastuff.items.machines;
 
+import io.github.thebusybiscuit.exoticgarden.ExoticGardenRecipeTypes;
+import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlockMachine;
+import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 import org.bukkit.Material;
 
+import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -15,6 +21,8 @@ import java.util.List;
 
 public class AutoKitchen extends AMachine {
 
+    private static final MultiBlockMachine mblock = (MultiBlockMachine) ExoticGardenRecipeTypes.KITCHEN.getMachine();
+
     public static final int[] BORDER = {4,5,6,7,8,13,31,40,41,42,43,44};
     private static final int[] BORDER_IN = {0,1,2,3,12,21,30,36,37,38,39};
     private static final int[] BORDER_OUT = {14,15,16,17,23,26,32,33,34,35};
@@ -22,6 +30,65 @@ public class AutoKitchen extends AMachine {
     public AutoKitchen(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(category, item, recipeType, recipe);
     }
+    @Override
+    public void tick(Block b) {
+        if (getCharge(b.getLocation()) < getEnergyConsumption()) {
+            return;
+        }
+        craftIfValid(b);
+    }
+
+    private void craftIfValid(Block block) {
+        BlockMenu menu = BlockStorage.getInventory(block);
+
+        // Make sure at least 1 slot is free
+        for (int outSlot : getOutputSlots()) {
+            ItemStack outItem = menu.getItemInSlot(outSlot);
+            if (outItem == null || outItem.getAmount() < outItem.getMaxStackSize()) {
+                break;
+            } else if (outSlot == getOutputSlots()[1]) {
+                return;
+            }
+        }
+
+        // Find matching recipe
+        for (ItemStack[] input : RecipeType.getRecipeInputList(mblock)) {
+            if (isCraftable(menu, input)) {
+                ItemStack output = RecipeType.getRecipeOutputList(mblock, input).clone();
+                craft(output, menu);
+                removeCharge(block.getLocation(), getEnergyConsumption());
+                return;
+            }
+        }
+        // we're only executing the last possible shaped recipe
+        // we don't want to allow this to be pressed instead of the default timer-based
+        // execution to prevent abuse and auto clickers
+    }
+
+    private boolean isCraftable(BlockMenu inv, ItemStack[] recipe) {
+        for (int i = 0; i < 9; i++) {
+            ItemStack item = inv.getItemInSlot(getInputSlots()[i]);
+            if ((item != null && item.getAmount() == 1)
+                    || !SlimefunUtils.isItemSimilar(inv.getItemInSlot(getInputSlots()[i]), recipe[i], true)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void craft(ItemStack output, BlockMenu inv) {
+        for (int i = 0; i < 9; i++) {
+            ItemStack item = inv.getItemInSlot(getInputSlots()[i]);
+
+            if (item != null && item.getType() != Material.AIR) {
+                inv.consumeItem(getInputSlots()[i]);
+            }
+        }
+
+        inv.pushItem(output, getOutputSlots());
+    }
+
 
     @Override
     public ItemStack getProgressBar() {
@@ -58,13 +125,4 @@ public class AutoKitchen extends AMachine {
     public String getMachineIdentifier() {
         return "AUTO_KITCHEN";
     }
-
-    @Override
-    public void registerDefaultRecipes() {
-        registerRecipe(400, new ItemStack(Material.CARROT), new ItemStack(Material.GOLDEN_CARROT));
-
-
-    }
-
-
 }
