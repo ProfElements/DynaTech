86de1541acff69d06c5586d8f8e7ca2d34dc749d
ProfElements <profelements1@gmail.com>
2020-12-21 15:30:01 -0600

    :art: work(AntigravityBubble): Remove unneeded imports

--- src/main/java/me/profelements/extrastuff/items/machines/AntigravityBubble.java
+++ src/main/java/me/profelements/extrastuff/items/machines/AntigravityBubble.java
@@ -1,15 +1,12 @@
 package me.profelements.extrastuff.items.machines;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
