5c9a7193b49433e89d177619e4cd4da0495da0a3
ProfElements <profelements1@gmail.com>
2021-01-23 12:52:13 -0600

    :bug: fix(DimensionalHome): Fix #20, use correct cscl import

--- src/main/java/me/profelements/dynatech/items/tools/DimensionalHome.java
+++ src/main/java/me/profelements/dynatech/items/tools/DimensionalHome.java
@@ -11,11 +11,11 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
-import me.mrCookieSlime.CSCoreLibPlugin.cscorelib2.data.PersistentDataAPI;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.cscorelib2.data.PersistentDataAPI;
 import me.profelements.dynatech.DynaTech;
 
 public class DimensionalHome extends SlimefunItem {
