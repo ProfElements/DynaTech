f0c54db2645c30f648ad26c6ef83385bffd7f234
waleks647 <78630612+waleks647@users.noreply.github.com>
2021-03-04 16:27:52 -0700

    Update GrowthChamberMK2.java

--- src/main/java/me/profelements/dynatech/items/electric/GrowthChamberMK2.java
+++ src/main/java/me/profelements/dynatech/items/electric/GrowthChamberMK2.java
@@ -1,6 +1,7 @@
 package me.profelements.dynatech.items.electric;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
