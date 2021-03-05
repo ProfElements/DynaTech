81a456ac0e477bf2f2b2ab487c1ab5cd6370d5ea
waleks647 <78630612+waleks647@users.noreply.github.com>
2021-03-04 20:05:50 -0700

    Update GrowthChamberMK2.java

--- src/main/java/me/profelements/dynatech/items/electric/GrowthChamberMK2.java
+++ src/main/java/me/profelements/dynatech/items/electric/GrowthChamberMK2.java
@@ -145,7 +145,7 @@ public class GrowthChamberMK2 extends AMachine {
     }
     @Override
     public int[] getOutputSlots() {
-        return new int[] {28,29,30,31,32,33,34,35,38,39,40,41,42,43,46,47,48,49,50,51,52,53};
+        return new int[] {28,29,30,31,32,33,34,37,38,39,40,41,42,43,46,47,48,49,50,51,52};
     }
 
     @Override
