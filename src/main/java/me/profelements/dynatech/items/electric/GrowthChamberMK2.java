a75691e88dd4690e299869e7c0cabac27c177b5f
waleks647 <78630612+waleks647@users.noreply.github.com>
2021-03-04 18:46:05 -0700

    Update GrowthChamberMK2.java

--- src/main/java/me/profelements/dynatech/items/electric/GrowthChamberMK2.java
+++ src/main/java/me/profelements/dynatech/items/electric/GrowthChamberMK2.java
@@ -38,7 +38,7 @@ public class GrowthChamberMK2 extends AMachine {
         registerRecipe(20, new ItemStack[] {new ItemStack(Material.MELON_SEEDS)}, new ItemStack[] {new ItemStack(Material.MELON , 8), new ItemStack(Material.MELON_SEEDS, 4)});
         registerRecipe(20, new ItemStack[] {new ItemStack(Material.PUMPKIN_SEEDS)}, new ItemStack[] {new ItemStack(Material.PUMPKIN , 8), new ItemStack(Material.PUMPKIN_SEEDS, 4)});
         registerRecipe(20, new ItemStack[] {new ItemStack(Material.BEETROOT_SEEDS)}, new ItemStack[] {new ItemStack(Material.BEETROOT , 12), new ItemStack(Material.BEETROOT_SEEDS, 4)});
-        registerRecipe(20, new ItemStack[] {new ItemStack(Material.WHEAT_SEEDS)}, new ItemStack[] {new ItemStack(Material.WHEAT , 12), new ItemStack(Material.WHEAT_SEEDS 4)});
+        registerRecipe(20, new ItemStack[] {new ItemStack(Material.WHEAT_SEEDS)}, new ItemStack[] {new ItemStack(Material.WHEAT , 12), new ItemStack(Material.WHEAT_SEEDS, 4)});
         registerRecipe(20, new ItemStack(Material.APPLE), new ItemStack(Material.APPLE, 4));
         registerRecipe(20, new ItemStack(Material.BROWN_MUSHROOM), new ItemStack(Material.BROWN_MUSHROOM, 12));
         registerRecipe(20, new ItemStack(Material.RED_MUSHROOM), new ItemStack(Material.RED_MUSHROOM, 12));
@@ -145,7 +145,7 @@ public class GrowthChamberMK2 extends AMachine {
     }
     @Override
     public int[] getOutputSlots() {
-        return new int[] {28,29,30,31,32,33,34,37,38,39,40,41,42,43,46,47,48,49,50,51,52};
+        return new int[] {28,29,30,31,32,33,34,35,38,39,40,41,42,43,46,47,48,49,50,51,52,53};
     }
 
     @Override
