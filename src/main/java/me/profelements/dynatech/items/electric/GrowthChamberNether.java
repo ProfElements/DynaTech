3a5fcf7eea28694ee745f7c9d33d17e7a36b67d4
waleks647 <78630612+waleks647@users.noreply.github.com>
2021-03-06 15:05:22 -0700

    Adding Nether Growth Chamber

---
+++ src/main/java/me/profelements/dynatech/items/electric/GrowthChamberNether.java
@@ -0,0 +1,47 @@
+package me.profelements.dynatech.items.electric;
+
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.MachineRecipe;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import me.profelements.dynatech.DynaTech;
+import me.profelements.dynatech.items.electric.abstracts.AMachine;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class GrowthChamberNether extends AMachine {
+
+    public GrowthChamberNether(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(category, item, recipeType, recipe);
+
+    }
+
+    @Override
+    protected void registerDefaultRecipes() {
+
+        registerRecipe(20, new ItemStack(Material.NETHER_WART), new ItemStack(Material.NETHER_WART, 4));
+        registerRecipe(20, new ItemStack(Material.WEEPING_VINES), new ItemStack(Material.WEEPING_VINES, 4));
+        registerRecipe(20, new ItemStack(Material.TWISTING_VINES), new ItemStack(Material.TWISTING_VINES, 4));
+        registerRecipe(20, new ItemStack(Material.CRIMSON_ROOTS), new ItemStack(Material.CRIMSON_ROOTS, 4));
+        registerRecipe(20, new ItemStack(Material.WARPED_ROOTS), new ItemStack(Material.WARPED_ROOTS, 4));
+        registerRecipe(20, new ItemStack(Material.NETHER_SPROUTS), new ItemStack(Material.NETHER_SPROUTS, 4));
+
+        registerRecipe(40, new ItemStack[] {new ItemStack(Material.CRIMSON_FUNGUS)}, new ItemStack[] {new ItemStack(Material.CRIMSON_FUNGUS, 2), new ItemStack(Material.CRIMSON_STEM, 6)});
+        registerRecipe(40, new ItemStack[] {new ItemStack(Material.WARPED_FUNGUS)}, new ItemStack[] {new ItemStack(Material.WARPED_FUNGUS, 2), new ItemStack(Material.WARPED_STEM, 6)});
+
+    }
+    
+    @Override
+    public ItemStack getProgressBar() {
+        return new ItemStack(Material.NETHERRACK);
+    }
+
+    @Override
+    public String getMachineIdentifier() {
+        return "GROWTH_CHAMBER_NETHER";
+    }
+
+}
