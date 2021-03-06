a6a3d5978c9e546c9bdb8f1e39d2a9693b90b6ad
waleks647 <78630612+waleks647@users.noreply.github.com>
2021-03-06 15:58:26 -0700

    Adding Ocean Growth Chamber
    It can revive dead corals and duplicate them
    Was thinking in naming it Water instead because lily pads don't generate on oceans but oceans sounds better, also to keep it in line with the biome names in the other GC's

---
+++ src/main/java/me/profelements/dynatech/items/electric/GrowthChamberOcean.java
@@ -0,0 +1,84 @@
+package me.profelements.dynatech.items.electric;
+
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.MachineRecipe;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import me.profelements.dynatech.DynaTech;
+import me.profelements.dynatech.items.electric.abstracts.AMachine;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class GrowthChamberOcean extends AMachine {
+
+    public GrowthChamberOcean(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(category, item, recipeType, recipe);
+
+    }
+
+    @Override
+    protected void registerDefaultRecipes() {
+
+        registerRecipe(20, new ItemStack(Material.LILY_PAD), new ItemStack(Material.LILY_PAD, 3));
+        registerRecipe(20, new ItemStack(Material.SEA_PICKLE), new ItemStack(Material.SEA_PICKLE, 3));
+        registerRecipe(20, new ItemStack(Material.SEAGRASS), new ItemStack(Material.SEAGRASS, 4));
+        registerRecipe(0, new ItemStack(Material.KELP), new ItemStack(Material.KELP, 3));
+    // Coral blocks
+        // Brings dead coral blocks back to life!
+        registerRecipe(20, new ItemStack(Material.DEAD_TUBE_CORAL_BLOCK), new ItemStack(Material.TUBE_CORAL_BLOCK, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_BRAIN_CORAL_BLOCK), new ItemStack(Material.BRAIN_CORAL_BLOCK, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_BUBBLE_CORAL_BLOCK), new ItemStack(Material.BUBBLE_CORAL_BLOCK, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_FIRE_CORAL_BLOCK), new ItemStack(Material.FIRE_CORAL_BLOCK, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_HORN_CORAL_BLOCK), new ItemStack(Material.HORN_CORAL_BLOCK, 1));
+        // Block duplication
+        registerRecipe(20, new ItemStack(Material.TUBE_CORAL_BLOCK), new ItemStack(Material.TUBE_CORAL_BLOCK, 2));
+        registerRecipe(20, new ItemStack(Material.BRAIN_CORAL_BLOCK), new ItemStack(Material.BRAIN_CORAL_BLOCK, 2));
+        registerRecipe(20, new ItemStack(Material.BUBBLE_CORAL_BLOCK), new ItemStack(Material.BUBBLE_CORAL_BLOCK, 2));
+        registerRecipe(20, new ItemStack(Material.FIRE_CORAL_BLOCK), new ItemStack(Material.FIRE_CORAL_BLOCK, 2));
+        registerRecipe(20, new ItemStack(Material.HORN_CORAL_BLOCK), new ItemStack(Material.HORN_CORAL_BLOCK, 2));
+
+    // Coral
+        // Revive for coral
+        registerRecipe(20, new ItemStack(Material.DEAD_TUBE_CORAL), new ItemStack(Material.TUBE_CORAL, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_BRAIN_CORAL), new ItemStack(Material.BRAIN_CORAL, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_BUBBLE_CORAL), new ItemStack(Material.BUBBLE_CORAL, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_FIRE_CORAL), new ItemStack(Material.FIRE_CORAL, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_HORN_CORAL), new ItemStack(Material.HORN_CORAL, 1));
+        // Coral duplication
+        registerRecipe(20, new ItemStack(Material.TUBE_CORAL), new ItemStack(Material.TUBE_CORAL, 2));
+        registerRecipe(20, new ItemStack(Material.BRAIN_CORAL), new ItemStack(Material.BRAIN_CORAL, 2));
+        registerRecipe(20, new ItemStack(Material.BUBBLE_CORAL), new ItemStack(Material.BUBBLE_CORAL, 2));
+        registerRecipe(20, new ItemStack(Material.FIRE_CORAL), new ItemStack(Material.FIRE_CORAL, 2));
+        registerRecipe(20, new ItemStack(Material.HORN_CORAL), new ItemStack(Material.HORN_CORAL, 2));
+
+    // Coral fans
+        // Medical attention for the fans
+        registerRecipe(20, new ItemStack(Material.DEAD_TUBE_CORAL_FAN), new ItemStack(Material.TUBE_CORAL_FAN, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_BRAIN_CORAL_FAN), new ItemStack(Material.BRAIN_CORAL_FAN, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_BUBBLE_CORAL_FAN), new ItemStack(Material.BUBBLE_CORAL_FAN, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_FIRE_CORAL_FAN), new ItemStack(Material.FIRE_CORAL_FAN, 1));
+        registerRecipe(20, new ItemStack(Material.DEAD_HORN_CORAL_FAN), new ItemStack(Material.HORN_CORAL_FAN, 1));
+        // Fan duplication
+        registerRecipe(20, new ItemStack(Material.TUBE_CORAL_FAN), new ItemStack(Material.TUBE_CORAL_FAN, 2));
+        registerRecipe(20, new ItemStack(Material.BRAIN_CORAL_FAN), new ItemStack(Material.BRAIN_CORAL_FAN, 2));
+        registerRecipe(20, new ItemStack(Material.BUBBLE_CORAL_FAN), new ItemStack(Material.BUBBLE_CORAL_FAN, 2));
+        registerRecipe(20, new ItemStack(Material.FIRE_CORAL_FAN), new ItemStack(Material.FIRE_CORAL_FAN, 2));
+        registerRecipe(20, new ItemStack(Material.HORN_CORAL_FAN), new ItemStack(Material.HORN_CORAL_FAN, 2));
+
+
+    }
+    
+    @Override
+    public ItemStack getProgressBar() {
+        return new ItemStack(Material.CONDUIT);
+    }
+
+    @Override
+    public String getMachineIdentifier() {
+        return "GROWTH_CHAMBER_OCEAN";
+    }
+
+}
