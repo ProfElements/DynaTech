cf5fb86f270a2d4c7d046b7a1925198e777aa0c8
ProfElements <profelements1@gmail.com>
2021-03-20 22:48:42 -0500

    :zap: work: Unhardcode world name and just use the world directly from

--- src/main/java/me/profelements/dynatech/DynaTech.java
+++ src/main/java/me/profelements/dynatech/DynaTech.java
@@ -3,6 +3,7 @@ package me.profelements.dynatech;
 import org.apache.commons.lang.Validate;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
@@ -47,8 +48,8 @@ public class DynaTech extends JavaPlugin implements SlimefunAddon {
         if (!cfg.getBoolean("options.disable-dimensionalhome-world")) {
             WorldCreator worldCreator = new WorldCreator("dimensionalhome");
             worldCreator.generator(new DimensionalHomeDimension());
-            worldCreator.createWorld();
-            new BlockStorage(Bukkit.getWorld("dimensionalhome"));
+            World dimensionalHome = worldCreator.createWorld();
+            new BlockStorage(dimensionalHome);
         }
 
         DynaTechItemsSetup.setup(this);
