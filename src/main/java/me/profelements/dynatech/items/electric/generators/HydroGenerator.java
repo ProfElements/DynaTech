c06f0e259c6bbf4692a2ea827b452e66db5748ba
ProfElements <profelements1@gmail.com>
2021-03-27 14:29:58 -0500

    :zap: fix(HydroGenerator && HydroTurbine): Increased speed and cached waterlogging. Thx Mooy1

--- src/main/java/me/profelements/dynatech/items/electric/generators/HydroGenerator.java
+++ src/main/java/me/profelements/dynatech/items/electric/generators/HydroGenerator.java
@@ -3,6 +3,7 @@ package me.profelements.dynatech.items.electric.generators;
 import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.profelements.dynatech.items.electric.abstracts.AMachineGenerator;
 
@@ -12,6 +13,7 @@ import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Waterlogged;
 import org.bukkit.inventory.ItemStack;
 
+import io.github.mooy1.infinityexpansion.infinitylib.PluginUtils;
 import io.github.thebusybiscuit.slimefun4.libraries.paperlib.PaperLib;
 
 import javax.annotation.Nonnull;
@@ -24,16 +26,23 @@ public class HydroGenerator extends AMachineGenerator {
 
     @Override
     public int getGeneratedOutput(@Nonnull Location location, @Nonnull Config config) {
-        BlockData blockData = PaperLib.getBlockState(location.getBlock(), false).getState().getBlockData();
+        if (PluginUtils.getCurrentTick()  % 10 == 0) {
+            BlockData blockData = PaperLib.getBlockState(location.getBlock(), false).getState().getBlockData();
 
-        if (blockData instanceof Waterlogged) {
-            Waterlogged data = (Waterlogged) blockData;
-            if (data.isWaterlogged()) {
-                return getEnergyProduction();
+            if (blockData instanceof Waterlogged) {
+                Waterlogged data = (Waterlogged) blockData;
+                if (data.isWaterlogged()) {
+                    BlockStorage.addBlockInfo(location, "waterlogged", String.valueOf(true));
+                    return getEnergyProduction();
+                } else {
+                    BlockStorage.addBlockInfo(location, "waterlogged", String.valueOf(false));
+                }
             }
-
+            return 0;
+        } else if (BlockStorage.getLocationInfo(location, "waterlogged").equals(String.valueOf(true))){
+            return getEnergyProduction();
         }
-        return 0;
+        return 0;        
     }
 
     @Override
