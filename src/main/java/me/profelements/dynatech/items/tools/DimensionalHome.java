4a0d4386e6526abdf8c412c42b296431dce38a97
ProfElements <profelements1@gmail.com>
2021-11-10 17:18:34 -0600

    refactor(DimensionalHome): :recycle: Fix #66, Fix #118, Fix #120. Completely overhaul Dimensional Home

--- src/main/java/me/profelements/dynatech/items/tools/DimensionalHome.java
+++ src/main/java/me/profelements/dynatech/items/tools/DimensionalHome.java
@@ -1,13 +1,18 @@
 package me.profelements.dynatech.items.tools;
 
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.data.persistent.PersistentDataAPI;
 import io.github.thebusybiscuit.slimefun4.libraries.paperlib.PaperLib;
+import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import me.profelements.dynatech.DynaTech;
+import me.profelements.dynatech.DynaTechItems;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
@@ -21,11 +26,10 @@ import java.util.List;
 
 public class DimensionalHome extends SlimefunItem {
     
-    private final NamespacedKey chunkId = new NamespacedKey(DynaTech.getInstance(), "chunk-id");
-
-    private int id = 1;
-    private boolean idSet = false;
-    private final World dimHomeWorld = Bukkit.getServer().getWorld("dimensionalhome");
+    private static final NamespacedKey CHUNK_KEY = new NamespacedKey(DynaTech.getInstance(), "chunk-key");
+    private static final World DIM_HOME_WORLD = Bukkit.getServer().getWorld("dimensionalhome");
+    private final Config CURRENT_HIGHEST_CHUNK_ID = new Config("plugins/DynaTech/current-chunk-highest.yml");
+    private int id = CURRENT_HIGHEST_CHUNK_ID.getInt("current-chunk-highest-id");
 
     public DimensionalHome(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
@@ -33,48 +37,31 @@ public class DimensionalHome extends SlimefunItem {
     }
 
     public ItemUseHandler onRightClick() {
-        return e -> {
-            Player p = e.getPlayer();
-            Location playerPrevLocation = p.getLocation();
+        return new ItemUseHandler() {
 
-            if (e.getPlayer().getWorld() != dimHomeWorld && idSet) {
+			@Override
+			public void onRightClick(PlayerRightClickEvent e) {
+			    e.cancel();
 
-                if (doesntContainNewChunkID(e.getItem())) {
-                    idSet = false;
+                Player p = e.getPlayer();
+                ItemStack item = e.getItem();
+                int chunkKey = PersistentDataAPI.getInt(item.getItemMeta(), CHUNK_KEY);
+
+                if (SlimefunUtils.isItemSimilar(item, DynaTechItems.DIMENSIONAL_HOME, true)) {
+                    if (chunkKey > 0) {
+                        if (p.getLocation().getWorld() != DIM_HOME_WORLD) {
+                            Location dimHomeLocation = new Location(DIM_HOME_WORLD, 16 * chunkKey + 8d, 65, 8);
+                            PaperLib.teleportAsync(p, dimHomeLocation);
+                        } else {
+                            PaperLib.teleportAsync(p, p.getBedSpawnLocation());        
+                        }
+                    } else {
+                        // Setup ChunkKey
+                        updateLore(item);
+                    }
                 }
-                
-                PaperLib.teleportAsync(p, new Location(dimHomeWorld, 16 * PersistentDataAPI.getInt(e.getItem().getItemMeta(), chunkId) + 8, 65, 8));
-            } else if (idSet) {
-
-                if (doesntContainNewChunkID(e.getItem())) {
-                    idSet = false;
-                }
-
-                PaperLib.teleportAsync(p,
-                    p.getWorld() != this.dimHomeWorld
-                        ? playerPrevLocation
-                        : p.getBedSpawnLocation() != null
-                        ? p.getBedSpawnLocation()
-                        : Bukkit.getWorlds().get(0).getSpawnLocation());
-            } else {
-                updateLore(e.getItem());
-            }
-
-            e.cancel();
+			} 
         };
-    }
-
-    protected boolean doesntContainNewChunkID(@Nonnull ItemStack item) {
-        ItemMeta im = item.getItemMeta();
-        List<String> lore = im.getLore();
-
-        for (String s : lore) {
-            if (s.contains("CHUNK ID: <id>")) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     private void updateLore(@Nonnull ItemStack item) {
@@ -85,9 +72,11 @@ public class DimensionalHome extends SlimefunItem {
             if (lore.get(line).contains("CHUNK ID: <id>")) {
                 id++;
                 lore.set(line, lore.get(line).replace("<id>", String.valueOf(id)));
-                PersistentDataAPI.setInt(im, chunkId, id);
-                        
-                idSet = true;
+                PersistentDataAPI.setInt(im, CHUNK_KEY, id);
+                
+                // THIS IS PROBABLY BAD AND A BAD WAY TO KEEP AN CHUNK ID
+                CURRENT_HIGHEST_CHUNK_ID.setValue("current-chunk-highest-id", id);
+                CURRENT_HIGHEST_CHUNK_ID.save();
             }
     
         }
