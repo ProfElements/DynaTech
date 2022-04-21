b5885e204bf32dd8955ab4e328bd0487acaa59bb
ProfElements <profelements1@gmail.com>
2022-04-20 23:04:36 -0500

    :art: chore: Clean up some code smells

--- src/main/java/me/profelements/dynatech/items/electric/BarbedWire.java
+++ src/main/java/me/profelements/dynatech/items/electric/BarbedWire.java
@@ -25,23 +25,19 @@ import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker;
 
 public class BarbedWire extends SlimefunItem implements EnergyNetComponent {
 
-    public final ItemSetting<Integer> ENTITY_PUSH_FORCE = new ItemSetting<>(this, "entity-push-force", getDefaultEntityPushForce());
-    public final ItemSetting<Integer> ENTITY_DETECT_RANGE = new ItemSetting<>(this, "entity-detect-range", getDefaultEntityDetectRange());
-    public final ItemSetting<List<EntityType>> ENTITY_WHITELIST = new ItemSetting<>(this, "enity-blacklist", getDefaultEntityWhitelist());
-    public final ItemSetting<Integer> BLOCK_ENERGY_CONSUMPTION = new ItemSetting<>(this, "block-energy-consumption", getDefaultEnergyConsumption());
+    private static final int DEFAULT_ENTITY_PUSH_FORCE = 10;
+    private static final int DEFAULT_ENTITY_DETECT_RANGE = 9;
+    private static final int DEFAULT_ENERGY_CONSUMPTION = 16;
+
+    private final ItemSetting<Integer> entityPushForce = new ItemSetting<>(this, "entity-push-force", DEFAULT_ENTITY_PUSH_FORCE);
+    private final ItemSetting<Integer> entityDetectRange = new ItemSetting<>(this, "entity-detect-range", DEFAULT_ENTITY_DETECT_RANGE);
+    private final ItemSetting<List<EntityType>> entityWhitelist = new ItemSetting<>(this, "enity-whitelist", getDefaultEntityWhitelist());
+    private final ItemSetting<Integer> blockEnergyConsumption = new ItemSetting<>(this, "block-energy-consumption", DEFAULT_ENERGY_CONSUMPTION);
 
     @ParametersAreNonnullByDefault
     public BarbedWire(ItemGroup group, SlimefunItemStack item,  RecipeType recipeType, ItemStack[] recipe) {
         super(group, item, recipeType, recipe);
-        addItemSetting(ENTITY_PUSH_FORCE, ENTITY_DETECT_RANGE, BLOCK_ENERGY_CONSUMPTION);
-    }
-
-    private int getDefaultEntityPushForce() {
-        return 10;
-    }
-
-    private int getDefaultEntityDetectRange() {
-        return 9;
+        addItemSetting(entityPushForce, entityDetectRange, entityWhitelist, blockEnergyConsumption);
     }
 
     private List<EntityType> getDefaultEntityWhitelist() {
@@ -68,9 +64,6 @@ public class BarbedWire extends SlimefunItem implements EnergyNetComponent {
         whitelist.add(EntityType.LLAMA_SPIT);
 
         return whitelist;
-    }
-    private int getDefaultEnergyConsumption() {
-        return 16;
     }
 
     @Override
@@ -101,18 +94,18 @@ public class BarbedWire extends SlimefunItem implements EnergyNetComponent {
     }
 
     protected void tick(Block block) {
-        int entityDetectRange = ENTITY_DETECT_RANGE.getValue();
-        int energyConsumption = BLOCK_ENERGY_CONSUMPTION.getValue();
-        List<EntityType> entityTypeWhitelist = ENTITY_WHITELIST.getValue();
+        int entityDetectionRange = entityDetectRange.getValue();
+        int energyConsumption = blockEnergyConsumption.getValue();
+        List<EntityType> entityTypeWhitelist = entityWhitelist.getValue();
         Location loc = block.getLocation();
         if (getCharge(loc) >= energyConsumption) {
             // Intentional. Remove it per tick whether or not it shields from mobs.
             removeCharge(loc, energyConsumption);
-            for (Entity e : block.getWorld().getNearbyEntities(loc, entityDetectRange, entityDetectRange, entityDetectRange)) {
+            for (Entity e : block.getWorld().getNearbyEntities(loc, entityDetectionRange, entityDetectionRange, entityDetectionRange)) {
                 if (entityTypeWhitelist.contains(e.getType())) {
 
                     /// Find the Direction Vector. normailze it. multiply by the push force. apply it to the entity.
-                    Vector launchDirection = loc.subtract(e.getLocation()).toVector().normalize().multiply(ENTITY_PUSH_FORCE.getValue());
+                    Vector launchDirection = loc.subtract(e.getLocation()).toVector().normalize().multiply(entityPushForce.getValue());
 
                     /// If any of the components are not finite just set it to zero.
                     if (NumberConversions.isFinite(launchDirection.getX()) && NumberConversions.isFinite(launchDirection.getY())
