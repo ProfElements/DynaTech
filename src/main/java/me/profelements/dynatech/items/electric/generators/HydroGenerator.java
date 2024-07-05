e7c67c04a08ee48994b2167dbc8f6a109a614696
ProfElements <profelements1@gmail.com>
2024-07-05 11:03:34 -0500

    :recycle: refactor(HydroGenerator): Make old water mill's basically useless. Force people to use the degrading water mills.

--- src/main/java/me/profelements/dynatech/items/electric/generators/HydroGenerator.java
+++ src/main/java/me/profelements/dynatech/items/electric/generators/HydroGenerator.java
@@ -1,89 +1,39 @@
 package me.profelements.dynatech.items.electric.generators;
 
-
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetProvider;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.blocks.BlockPosition;
-import io.github.thebusybiscuit.slimefun4.libraries.paperlib.PaperLib;
-import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.Waterlogged;
+import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
+import me.profelements.dynatech.DynaTechItems;
+
+import java.util.List;
+
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-import javax.annotation.Nonnull;
-import java.util.concurrent.TimeUnit;
-
-public class HydroGenerator extends SlimefunItem implements EnergyNetProvider {
+public class HydroGenerator extends SlimefunItem {
 
     private final int energy;
 
-    private final LoadingCache<BlockPosition, Integer> cachedGeneration = CacheBuilder.newBuilder()
-        .refreshAfterWrite(1, TimeUnit.MINUTES)
-        .expireAfterAccess(3, TimeUnit.MINUTES)
-        .build(new CacheLoader<>() {
-            @Override
-            public Integer load(@Nonnull BlockPosition key) {
-                return fetchOutputForBlock(key);
-            }
-        });
-
     public HydroGenerator(ItemGroup itemGroup, int energy, SlimefunItemStack item, RecipeType recipeType,
-                          ItemStack[] recipe) {
+            ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
 
         this.energy = energy;
+
+        addItemHandler(onBlockBreak());
     }
 
-    @Override
-    public int getGeneratedOutput(@Nonnull Location location, @Nonnull Config config) {
-        final BlockPosition pos = new BlockPosition(location);
-        Integer i = cachedGeneration.getIfPresent(pos);
-
-        if (i != null) {
-            return i;
-        } else {
-            int output = fetchOutputForBlock(pos);
-            cachedGeneration.put(pos, output);
-            return output;
-        }
-    }
-
-    private int fetchOutputForBlock(@Nonnull BlockPosition position) {
-        final Block b = position.getBlock();
-
-        if (b.getType() == Material.COBBLESTONE_WALL || b.getType() == Material.PRISMARINE_WALL) {
-            BlockData blockData = PaperLib.getBlockState(b, false).getState().getBlockData();
-            if (blockData instanceof Waterlogged data && data.isWaterlogged()) {
-                return getEnergyProduction();
-            } else {
-                // Block has been removed, invalidate the cache
-                cachedGeneration.invalidate(position);
-                return 0;
+    private BlockBreakHandler onBlockBreak() {
+        return new BlockBreakHandler(false, false) {
+            @Override
+            public void onPlayerBreak(BlockBreakEvent event, ItemStack arg1, List<ItemStack> arg2) {
+                arg2.clear();
+                event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(),
+                        DynaTechItems.DEGRADED_WATER_MILL);
             }
-        }
-        return 0;
+        };
     }
 
-    @Override
-    public boolean isChargeable() {
-        return false;
-    }
-
-    public int getEnergyProduction() {
-        return energy;
-    }
-
-    @Override
-    public int getCapacity() {
-        return 0;
-    }
 }
